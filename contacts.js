54edde799c16cc0170272b93ae64f1e6edcd1608
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-10-27 13:33:55 +0300

    1

--- contacts.js
+++ contacts.js
@@ -1,35 +1,53 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
-import { contacts } from "./db/contacts.json";
-// const path = require('path').promises;
-// const fs = require('fs').promises;
-// const contactsPath = contacts;
+const fs = require("fs/promises");
+const path = require("path");
 
 
-// fs.readFile('readme.txt')
-//   .then(data => console.log(data.toString()))
-//   .catch(err => console.log(err.message));
-  
-// fs.writeFile('readme.txt')
-//     .then(data => console.log(data.toString()))
-//     .catch(err => console.log(err.message));
-  
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+const { v4: uuidv4 } = require("uuid");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((contact) => contact.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removeContact;
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
